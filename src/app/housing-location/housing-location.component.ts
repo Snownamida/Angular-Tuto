59fb4f1c11b4468b50af6bffc85d97cdb94d5ed3
Jixiang SUN <snownamida@outlook.com>
2025-03-24 16:16:34 +0100

    向组件添加一个输入参数

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
+import { HousingLocation } from "../housinglocation";
 @Component({
   selector: "app-housing-location",
   imports: [CommonModule],
   template: ` <p>housing-location works!</p> `,
   styleUrls: ["./housing-location.component.css"],
 })
-export class HousingLocationComponent {}
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
